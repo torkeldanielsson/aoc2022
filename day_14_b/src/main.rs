d3e4a41819bb0edc47b6fc429c03f3a21e6d50f0
Torkel <torkel@voysys.se>
2022-12-14 08:38:28 +0100

    day 14

---
+++ day_14_b/src/main.rs
@@ -0,0 +1,71 @@
+use glam::ivec2;
+use std::{collections::HashSet, error::Error, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("input")?;
+
+    let mut map = HashSet::new();
+
+    for line in input.lines() {
+        for w in line
+            .split(" -> ")
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|s| s.split_once(',').unwrap())
+            .map(|t| ivec2(t.0.parse::<i32>().unwrap(), t.1.parse::<i32>().unwrap()))
+            .collect::<Vec<_>>()
+            .as_slice()
+            .windows(2)
+        {
+            let delta = match (w[0], w[1]) {
+                (a, b) if a.x == b.x && a.y > b.y => ivec2(0, -1),
+                (a, b) if a.x == b.x && a.y < b.y => ivec2(0, 1),
+                (a, b) if a.y == b.y && a.x > b.x => ivec2(-1, 0),
+                (a, b) if a.y == b.y && a.x < b.x => ivec2(1, 0),
+                _ => panic!("?"),
+            };
+            let mut p = w[0];
+            map.insert(p);
+            while p != w[1] {
+                p += delta;
+                map.insert(p);
+            }
+        }
+    }
+
+    let count_before_sand = map.len();
+    let mut lowest_point = 0;
+    for p in &map {
+        lowest_point = std::cmp::max(lowest_point, p.y);
+    }
+    lowest_point += 2;
+
+    'outer: loop {
+        let mut sand = ivec2(500, 0);
+
+        'inner: loop {
+            if !map.contains(&(sand + ivec2(0, 1))) {
+                sand += ivec2(0, 1);
+            } else if !map.contains(&(sand + ivec2(-1, 1))) {
+                sand += ivec2(-1, 1);
+            } else if !map.contains(&(sand + ivec2(1, 1))) {
+                sand += ivec2(1, 1);
+            } else {
+                map.insert(sand);
+                break 'inner;
+            }
+            if sand.y + 1 == lowest_point {
+                map.insert(sand);
+                break 'inner;
+            }
+        }
+
+        if map.contains(&ivec2(500, 0)) {
+            break 'outer;
+        }
+    }
+
+    println!("{}", map.len() - count_before_sand);
+
+    Ok(())
+}
