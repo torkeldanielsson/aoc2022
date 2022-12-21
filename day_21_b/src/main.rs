82116618db96e40410ff185bf5e0ce2d6872626a
Torkel <torkel@voysys.se>
2022-12-21 09:08:17 +0100

    hmm

--- day_21_b/src/main.rs
+++ day_21_b/src/main.rs
@@ -1,13 +1,32 @@
-use std::{error::Error, fs};
+use std::{collections::BTreeMap, error::Error, fs};
 
 // root: tcmj + qggp
-// humn: 3640
+
+fn pull(
+    tag: &str,
+    equations: &BTreeMap<&str, (&str, &str, &str)>,
+    known_numbers: &BTreeMap<&str, i64>,
+) -> String {
+    if let Some(n) = known_numbers.get(tag) {
+        return format!("{n}");
+    }
+    if let Some(eq) = equations.get(tag) {
+        return format!(
+            "({} {} {})",
+            pull(eq.0, equations, known_numbers),
+            eq.1,
+            pull(eq.2, equations, known_numbers),
+        );
+    }
+    panic!()
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    /*
     let input = fs::read_to_string("input")?.replace(':', "");
 
-    let mut known_numbers = std::collections::BTreeMap::new();
-    let mut equations = Vec::new();
+    let mut known_numbers = BTreeMap::new();
+    let mut v_equations = Vec::new();
 
     for line in input
         .lines()
@@ -18,21 +37,22 @@ fn main() -> Result<(), Box<dyn Error>> {
                 known_numbers.insert(line[0], line[1].parse::<i64>().unwrap());
             }
             4 => {
-                equations.push((line[0], line[1], line[2], line[3]));
+                v_equations.push((line[0], line[1], line[2], line[3]));
             }
             _ => panic!(),
         }
     }
 
-    let mut new_known_numbers = known_numbers.clone();
-    let mut new_equations = equations.clone();
-    new_known_numbers.insert("humn", -700);
+    loop {
+        let mut simplified = false;
 
-    while !new_equations.is_empty() {
-        for i in 0..new_equations.len() {
-            let eq = &new_equations[i];
-            if let (Some(a), Some(b)) = (new_known_numbers.get(eq.1), new_known_numbers.get(eq.3)) {
-                new_known_numbers.insert(
+        for i in 0..v_equations.len() {
+            let eq = &v_equations[i];
+            if eq.1 == "humn" || eq.3 == "humn" {
+                continue;
+            }
+            if let (Some(a), Some(b)) = (known_numbers.get(eq.1), known_numbers.get(eq.3)) {
+                known_numbers.insert(
                     eq.0,
                     match eq.2 {
                         "+" => a + b,
@@ -42,18 +62,106 @@ fn main() -> Result<(), Box<dyn Error>> {
                         _ => panic!(),
                     },
                 );
-                new_equations.remove(i);
+                simplified = true;
+                v_equations.remove(i);
                 break;
             }
         }
+
+        if !simplified {
+            break;
+        }
     }
 
-    println!(
-        "humn: {} => {} == {}",
-        new_known_numbers["humn"], new_known_numbers["tcmj"], new_known_numbers["qggp"]
-    );
+    let mut equations = BTreeMap::new();
+    for ve in v_equations {
+        equations.insert(ve.0, (ve.1, ve.2, ve.3));
+    }
+
+    println!("tcmj: {}", pull("tcmj", &equations, &known_numbers));
+    println!("qggp: {}", pull("qggp", &equations, &known_numbers));
+    */
+
+    for i in 0..1_000_000_000_000 as i64 {
+        if i % 100_000_000 == 0 {
+            println!("{i}");
+        }
+
+        if ((634
+            + (101692068627800
+                - (((((2
+                    * (516
+                        + ((868
+                            + (((((2
+                                * (((((99
+                                    + ((((2
+                                        * ((((((((((((((5
+                                            * (((268
+                                                + (((76
+                                                    + ((((((((837
+                                                        + ((((337
+                                                            + ((((2
+                                                                * (((674
+                                                                    + ((((137
+                                                                        + i)
+                                                                        / 3)
+                                                                        - 467)
+                                                                        * 81))
+                                                                    / 2)
+                                                                    - 387))
+                                                                - 865)
+                                                                + 365)
+                                                                / 8))
+                                                            * 11)
+                                                            - 215)
+                                                            / 4))
+                                                        * 4)
+                                                        + 998)
+                                                        * 2)
+                                                        - 794)
+                                                        / 2)
+                                                        - 602)
+                                                        * 2))
+                                                    / 2)
+                                                    - 414))
+                                                / 7)
+                                                + 614))
+                                            + 81)
+                                            * 2)
+                                            - 562)
+                                            / 6)
+                                            - 829)
+                                            * 3)
+                                            + 173)
+                                            / 7)
+                                            + 871)
+                                            / 6)
+                                            - 888)
+                                            * 40)
+                                            - 128))
+                                        + 519)
+                                        + 941)
+                                        / 12))
+                                    * 9)
+                                    - 582)
+                                    / 4)
+                                    + 844))
+                                - 191)
+                                / 3)
+                                - 386)
+                                / 5))
+                            * 15)))
+                    - 338)
+                    / 4)
+                    + 88)
+                    * 3)))
+            / 11)
+            == 792784087587
+        {
+            println!("!! {i}");
+            return Ok(());
+        }
+    }
 
     Ok(())
 }
-// 792784087587
-// 9244733517335
